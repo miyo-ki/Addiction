9d713cfb5e9a1c87cd8ca713d3490ded5fedc94f
Mona Bourgeron <bourgeronmona@gmail.com>
2026-04-01 10:38:10 +0200

    first push mobile page

--- site_web/mobile_addiction.php
+++ site_web/mobile_addiction.php
@@ -3,38 +3,33 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Addiction</title>
+  <title>Addiction aux Téléphones Portables</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <header>
-    <nav>
-      <h1 class="logo">Addiction</h1>
+      <nav>
+      <h1 class="logo">Addiction aux Téléphones Portables</h1>
       <ul>
-        <li class="active">Accueil </li>
+        <li class="active">Accueil</li>
         <li><a href="carte.php">Tester votre taux d'addiction</a></li>
         <li><a href="apropos.html">Informations sur les Addictions</a></li>
         <li><a href="contact.html">Contact</a></li>
       </ul>
     </nav>
   </header>
-
   <main>
     <section class="hero">
-      <div class="overlay"></div>
-      <div class="content">
-        <h1>Informe toi, Évalue ton problème, Aide toi</h1>
-        <div class="buttons">
-          <a href="quiz.php" class="btn">Éffectue un test pour évaluer ton niveau d'addiction</a>
-          <a href="informations.php" class="btn">Informe toi</a>
-        </div>
+      <div class="explication">
+        <h2>Comprendre l'Addiction aux Téléphones Portables</h2>
+        <p>Bienvenue sur notre page dédié à l'addiction aux téléphones portables. </p>
+        <p>Nous sommes là pour vous aider à comprendre les risques liés à une utilisation excessive de votre téléphone et à évaluer votre dépendance.</p>
+        <p>Explorez nos ressources, testez votre niveau d'addiction et découvrez des conseils pratiques pour retrouver un équilibre sain avec la technologie.</p>
       </div>
     </section>
-  </main>
-
+</main>
     <footer>
         <p>&copy; 2026 Addictions. Tous droits réservés. | Projet réalisé par des étudiants de l'Université Paul Valéry</p>
     </footer>
-   
 </body>
 </html>
