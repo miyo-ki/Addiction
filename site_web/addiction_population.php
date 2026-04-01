8af877efd54619f2fb258ad7cb232d17075faa88
Sidney Dachez <sidsmog@gmail.com>
2026-04-01 09:42:42 +0200

    ajout site

---
+++ site_web/addiction_population.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Addiction</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <nav>
+      <h1 class="logo">Addiction</h1>
+      <ul>
+        <li class="active">Accueil </li>
+        <li><a href="carte.php">Tester votre taux d'addiction</a></li>
+        <li><a href="apropos.html">Informations sur les Addictions</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <section class="hero">
+      <div class="overlay"></div>
+      <div class="content">
+        <h1>Informe toi, Évalue ton problème, Aide toi</h1>
+        <div class="buttons">
+          <a href="quiz.php" class="btn">Éffectue un test pour évaluer ton niveau d'addiction</a>
+          <a href="informations.php" class="btn">Informe toi</a>
+        </div>
+      </div>
+    </section>
+  </main>
+
+    <footer>
+        <p>&copy; 2026 Addictions. Tous droits réservés. | Projet réalisé par des étudiants de l'Université Paul Valéry</p>
+    </footer>
+   
+</body>
+</html>
